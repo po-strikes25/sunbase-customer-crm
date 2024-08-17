7c43c2332c6b78cc5ea39774a41f4552375a7473
Shilpi <svvar31@gmail.com>
2024-08-17 21:21:13 +0530

    sql file for schema for in-memory & password encoder

--- crm_customer/src/main/java/com/crm/crm_customer/config/SunbaseWebSecurityConfig.java
+++ crm_customer/src/main/java/com/crm/crm_customer/config/SunbaseWebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.crm.crm_customer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,24 +25,42 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SunbaseWebSecurityConfig {
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests
+                        // .requestMatchers("/crm-api/**").permitAll()
+                        .anyRequest().authenticated());
         http.sessionManagement((session) -> session.sessionCreationPolicy((SessionCreationPolicy.STATELESS)));
         http.httpBasic(withDefaults());
+        // http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        // http.csrf((csrf) -> csrf.disable());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-                                .password("{noop}admin_2024")
-                                .roles("ADMIN")
-                                .build();
-        UserDetails user_1 = User.withUsername("user_1")
-                                .password("{noop}user_1_2024")
-                                .roles("USER")
-                                .build();
-        return new InMemoryUserDetailsManager(admin, user_1);
+        UserDetails it_security = User.withUsername("it_security")
+                                    .password(passwordEncoder().encode("it_security_2024"))
+                                    .roles("ADMIN")
+                                    .build();
+        UserDetails user_2      = User.withUsername("user_2")
+                                    .password("{noop}user_2_2024")
+                                    .roles("USER")
+                                    .build();
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(it_security);
+        userDetailsManager.createUser(user_2);
+
+        return userDetailsManager;
+        // return new InMemoryUserDetailsManager(admin, user_1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
